0b99ee0bb422f40ca6c0cedebd318562d491d1c4
rdorrity <37189242+rdorrity@users.noreply.github.com>
2019-07-30 22:33:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ app.get("/api/displayFavorites", function(req, res) {
 });//displayFavorites
 
 //server listening
-app.listen(process.env.PORT || 8081, 'us-cdbr-iron-east-02.cleardb.net', function(){
+app.listen(process.env.PORT || 8081, process.env.IP, function(){
    console.log("Express Server is Running...")
 });
